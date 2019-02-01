524f8eb367de4e835686d2f5f2b95f9e4e033946
Mirko Felice <mirko.felice@studio.unibo.it>
2019-02-01 15:47:24 +0100

    Add base interfaces

--- src/model/Entity.java
+++ src/model/Entity.java
@@ -1,8 +1,15 @@
 package model;
 
 /**
- *
+ * an interface that represents an Entity.
  */
 public interface Entity {
 
+    /**
+     * @param x the horizontal position.
+     * @param y the vertical position.
+     * 
+     * method to move an Entity.
+     */
+    void move(int x, int y);
 }
---
+++ src/model/Shooter.java
@@ -0,0 +1,12 @@
+package model;
+/**
+ * an interface that represents a shooter.
+ *
+ */
+public interface Shooter {
+
+    /**
+     * method to shoot.
+     */
+    void shoot();
+}
